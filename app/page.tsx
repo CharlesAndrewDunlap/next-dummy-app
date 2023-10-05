f35f9eb3b2cd38cfa6a0d018fb56d90938e6ca26
Jose Velasco <josevelasco1022@gmail.com>
2023-10-05 16:57:27 -0700

    client vs server fetch request

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import CatPage from './page1';
 
+import { Card } from './components/Card'
+import { Create } from './components/Create'
+
+
 const { trace }  = require("@opentelemetry/api");
 const { BasicTracerProvider, SimpleSpanProcessor, ConsoleSpanExporter }  = require("@opentelemetry/sdk-trace-base");
 const { OTLPTraceExporter } =  require('@opentelemetry/exporter-trace-otlp-http');
 
+
 const collectorOptions = {
   url: 'http://localhost:9999/otel', 
 };
-
 
 const provider = new BasicTracerProvider();
 provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
@@ -16,7 +20,6 @@ const exporter = new OTLPTraceExporter(collectorOptions);
 provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
 trace.setGlobalTracerProvider(provider);
 provider.register();
-
 
 
 
@@ -31,18 +34,28 @@ export default async function Home() {
   });
   const randomUserData = await randomUser.json();
 
-  const errorData = await fetch('http://www.google.com/404', {
-    cache: 'no-store',
-  });
-  
+
+  const comments = await fetch('http://localhost:3001/api/comment', { cache: 'no-store' });
+  const data = await comments.json();
 
   
+
   return (
     <div className="content">
+      <div className="create-form">
+        <Create />
+      </div>
+      <div className="cards">
+        {data?.map((comment: any) => {
+          return <Card key={comment._id} comment={comment.comment} />
+        })}
+      </div>
       <div>{parsedData.first_name}</div>
       <div>{randomUserData.results[0].gender}</div>
       <CatPage/>
     </div>
-  );
+    
+  )
 
 }
+
--- app/page1.tsx
+++ app/page1.tsx
@@ -1,30 +1,17 @@
-// export default async function CatPage() {
+export default async function CatPage() {
 
-//   const userData = await fetch('https://cat-fact.herokuapp.com/facts', {
-//     cache: 'no-store',
-//   });
-//   const parsedData = await userData.json();
+  const userData = await fetch('https://cat-fact.herokuapp.com/facts', {
+    cache: 'no-store',
+  });
+  const parsedData = await userData.json();
 
   
 
-//   return (
-//     <div className="content">
-//       <div>{parsedData[0].text}</div>
-//     </div>
-//   );
-// }
-
-
-
-export default async function CatPage() {
-
-  const userData = await fetch('https://example.com/non-existent-page', {
-    cache: 'no-store',
-  });
-;
-
   return (
     <div className="content">
+      <div>{parsedData[0].text}</div>
     </div>
   );
 }
+
+
